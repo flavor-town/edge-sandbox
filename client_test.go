51fb3174ad37e6a34c269f3e9eefc5af4bd29433
Flavor Town <schauhan@polygon.technology>
2023-04-26 14:39:46 -0700

    tests: adding more debugging tests

--- client_test.go
+++ client_test.go
@@ -10,24 +10,33 @@ import (
 	"testing"
 	"time"
 
+	polytypes "github.com/0xPolygon/polygon-edge/types"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/maticnetwork/polygon-cli/rpctypes"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/umbracle/fastrlp"
+	"golang.org/x/crypto/sha3"
 
 	"github.com/joho/godotenv"
 )
 
+type Transaction struct {
+	Hash string `json:"hash"`
+	Type string `json:"type"`
+}
+
+// type Transaction edgetypes.Transaction
+
 type RPCResult struct {
-	Number       string      `json:"number"`
-	Hash         common.Hash `json:"hash"`
-	ParentHash   common.Hash `json:"parentHash"`
-	Transactions []struct {
-		Hash string `json:"hash"`
-	} `json:"transactions"`
+	Number       string         `json:"number"`
+	Hash         common.Hash    `json:"hash"`
+	ParentHash   common.Hash    `json:"parentHash"`
+	Transactions []*Transaction `json:"transactions"`
 }
 
 type EdgeTest struct {
@@ -168,9 +177,9 @@ func TestTxnHash(t *testing.T) {
 
 	// Build Transaction
 	var data []byte
-	tx := types.NewTransaction(nonce, toAddress, value, gasLimit, gasPrice, data)
+	tx := ethtypes.NewTransaction(nonce, toAddress, value, gasLimit, gasPrice, data)
 	chainID, _ := edge.evmClient.NetworkID(edge.ctx)
-	signedTx, _ := types.SignTx(tx, types.NewEIP155Signer(chainID), privateKey)
+	signedTx, _ := ethtypes.SignTx(tx, ethtypes.NewEIP155Signer(chainID), privateKey)
 
 	// Submit Txn
 	err = edge.evmClient.SendTransaction(edge.ctx, signedTx)
@@ -235,7 +244,7 @@ func TestAllBlocks(t *testing.T) {
 		for _, m := range evmBlock.Transactions() {
 			log.Printf("EVM: Block %d has tx hash %s", i, m.Hash().String())
 		}
-		err = edge.rpcClient.CallContext(edge.ctx, &rpcBlock, "eth_getBlockByNumber", "0x"+bigBlockNumber.Text(16), true)
+		err = edge.rpcClient.CallContext(edge.ctx, &rpcBlock, "eth_getBlockByNumber", encodeBig(bigBlockNumber), true)
 		if err != nil {
 			log.Fatalf("RPC: getBlockByNumber: %+v", err)
 		}
@@ -247,4 +256,148 @@ func TestAllBlocks(t *testing.T) {
 			assert.Equal(t, evmBlock.Transactions()[n].Hash().String(), rpcBlock.Transactions[n].Hash)
 		}
 	}
+}
+
+// EncodeBig encodes bigint as a hex string with 0x prefix.
+func encodeBig(bigint *big.Int) string {
+	if sign := bigint.Sign(); sign == 0 {
+		return "0x0"
+	} else if sign > 0 {
+		return "0x" + bigint.Text(16)
+	} else {
+		return "-0x" + bigint.Text(16)[1:]
+	}
+}
+
+// Running on this commit: https://github.com/0xPolygon/polygon-edge/blob/a0ce3163e2f3b0fd4d0886b1de32197107b26757/types/rlp_marshal.go#L32
+func TestEpochBlocks(t *testing.T) {
+	edge := NewEdgeTest()
+	epochSize := big.NewInt(10) // Every epoch is configured to be 10 blocks
+
+	var rpcBlock rpctypes.RawBlockResponse
+
+	evmBlock, _ := edge.evmClient.BlockByNumber(edge.ctx, epochSize)
+	log.Printf("EVM: BlockNumber %d has %d transactions", evmBlock.Number(), evmBlock.Transactions().Len())
+	for _, m := range evmBlock.Transactions() {
+		log.Printf("EVM: Block %d has tx hash %s", evmBlock.Number(), m.Hash().String())
+	}
+
+	edge.rpcClient.CallContext(edge.ctx, &rpcBlock, "eth_getBlockByNumber", encodeBig(evmBlock.Number()), true)
+
+	var evmTxn *ethtypes.Transaction
+	var rpcTxn *rpctypes.RawTransactionResponse
+	var polyTxn *polytypes.Transaction
+
+	for n := 0; n < evmBlock.Transactions().Len(); n++ {
+		evmTxn = evmBlock.Transactions()[n]
+		rpcTxn = &rpcBlock.Transactions[n]
+
+		log.Printf("ethClient hash\t\t\t: %+v", evmTxn.Hash().String())         // Use the txnHash given by the ethClient
+		log.Printf("RawHash from RPC \t\t: %+v", rpcTxn.Hash.ToHash().String()) // Raw Hash from RPC node
+
+		log.Println("==================== RPC --> PolyTxn ====================") // Should technically equal RawHash from RPC.
+		polyTxn = rawToPolyTxn(rpcTxn)
+		log.Printf("ArenaHash \t\t\t\t: %+v", arenaHash(polyTxn))                         // Convert rpcTxn -> polyTxn and manually generate hash
+		log.Printf("edgeTxn.ComputeHash(): \t: %+v", polyTxn.ComputeHash().Hash.String()) // Use built-in function (should be the same as above)
+
+		log.Println("==================== Geth --> PolyTxn ====================")
+		polyTxn = gethToEdgeTxn(evmTxn)
+		log.Printf("ArenaHash \t\t\t\t: %+v", arenaHash(polyTxn))                         // Convert rpcTxn -> polyTxn and manually generate hash
+		log.Printf("edgeTxn.ComputeHash(): \t: %+v", polyTxn.ComputeHash().Hash.String()) // Use built-in function (should be the same as above)
+
+		assert.Equal(t, evmTxn.Hash().String(), rpcTxn.Hash.ToHash().String())
+	}
+}
+
+// Converts raw transaction response to a polyedge transaction type.
+func rawToPolyTxn(r *rpctypes.RawTransactionResponse) *polytypes.Transaction {
+	toAddress := polytypes.BytesToAddress(r.To.ToAddress().Bytes())
+	p := &polytypes.Transaction{
+		Nonce:    r.Nonce.ToUint64(),
+		GasPrice: r.GasPrice.ToBigInt(),
+		// EIP1559 things
+		// GasTipCap: r.GasTipCap.ToBigInt(),
+		// GasFeeCap: r.GasFeeCap.ToBigInt(),
+		Gas:   r.Gas.ToUint64(),
+		To:    &toAddress,
+		Value: r.Value.ToBigInt(),
+		Input: r.Input.ToBytes(),
+		V:     r.V.ToBigInt(),
+		R:     r.R.ToBigInt(),
+		S:     r.S.ToBigInt(),
+		// From:  polytypes.Address(r.From.ToAddress()),
+		Type: polytypes.TxType(r.Type.ToInt64()),
+	}
+
+	return p
+}
+
+// Takes a geth txn and converts it into a polygon txn type.
+func gethToEdgeTxn(gt *ethtypes.Transaction) *polytypes.Transaction {
+	toAddress := polytypes.BytesToAddress(gt.To().Bytes())
+
+	p := &polytypes.Transaction{
+		Nonce:    gt.Nonce(),
+		GasPrice: gt.GasPrice(),
+		// EIP1559 things
+		// GasTipCap: gt.GasTipCap.ToBigInt(),
+		// GasFeeCap: gt.GasFeeCap.ToBigInt(),
+		Gas:   gt.Gas(),
+		To:    &toAddress,
+		Value: gt.Value(),
+		Input: gt.Data(),
+		V:     big.NewInt(0), //gt.V(), 	   // Missing
+		R:     big.NewInt(0), //gt.R(),        // Missing
+		S:     big.NewInt(0), //gt.S(),        // Missing
+		// From:  polytypes.Address(gt.From.ToAddress()), // Missing
+		Type: polytypes.TxType(gt.Type()),
+	}
+	return p
+}
+
+// Copy-pasta of hashing algo used in polygon-edge codebase
+// Data that affects the hash
+// Nonce
+// GasPrice
+// Gas
+// To (optional: nil)
+// Value
+// Input
+// V, R, S
+// Type  (0x0 = legacyTx)
+// From  ^ only if Type=StateTx (which epoch commits are not)
+
+func arenaHash(t *polytypes.Transaction) (retHash common.Hash) {
+	arena := &fastrlp.Arena{}
+
+	vv := arena.NewArray()
+
+	vv.Set(arena.NewUint(t.Nonce))      //0
+	vv.Set(arena.NewBigInt(t.GasPrice)) // 0
+	vv.Set(arena.NewUint(t.Gas))        // 1000000
+
+	// Address may be empty
+	if t.To != nil {
+		vv.Set(arena.NewBytes((*t.To).Bytes())) // 0x0101
+	} else {
+		vv.Set(arena.NewNull())
+	}
+
+	vv.Set(arena.NewBigInt(t.Value))    // 0
+	vv.Set(arena.NewCopyBytes(t.Input)) // len = 548
+
+	// signature values
+	vv.Set(arena.NewBigInt(t.V))
+	vv.Set(arena.NewBigInt(t.R))
+	vv.Set(arena.NewBigInt(t.S))
+
+	if t.Type == polytypes.StateTx { // 0x0 - Legacy TXN
+		vv.Set(arena.NewBytes((t.From).Bytes()))
+	}
+	sha := sha3.NewLegacyKeccak256()
+	buf := vv.MarshalTo(nil)
+	sha.Write(buf)
+
+	retHash.SetBytes(sha.Sum(nil))
+	return
 }
